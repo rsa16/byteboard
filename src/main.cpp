ef85ad98a5aef5d722dd0a69949b766fcaa037b6
rsa16 <sipnet66@outlook.com>
2024-02-09 19:27:37 -0500

    loading code for qml

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,33 @@
-#include <QTextStream>
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
 
-int main() {
-    QTextStream(stdout) << "Hello, world!" << Qt::endl;
-    return 0;
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    const QUrl url(u"qrc:/data/main.qml"_qs);
+
+    // On object creation of QML Application engine, once our app is fully initialized, 
+    QObject::connect(
+        &engine, &QQmlApplicationEngine::objectCreated, &app,
+        [url](QObject *obj, const QUrl &objUrl)
+        {
+            if (!obj && url == objUrl)
+                QCoreApplication::exit(-1);
+        },
+        Qt::QueuedConnection);
+
+    // later code
+    // engine.addImportPath(QCoreApplication::applicationDirPath() + "/qml");
+    // engine.addImportPath(":/");
+
+    engine.load(url);
+
+    if (engine.rootObjects().isEmpty())
+    {
+        return -1;
+    }
+
+    return app.exec();
 }
